f737ce9823ddc1ea2dc097ea9fe8b315e29edaf7
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-06-24 16:54:59 +0530

    Create Question 4.sql

---
+++ sql-practice/Question 4.sql
@@ -0,0 +1,4 @@
+-- Problem: Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+-- Difficulty: Easy
+
+SELECT first_name, last_name FROM patients where weight between 100 and 120 ;
