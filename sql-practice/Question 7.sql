2fd83939c35e04d1ca456bf16109a16ee03c6392
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-06-24 16:59:34 +0530

    Create Question 7.sql

---
+++ sql-practice/Question 7.sql
@@ -0,0 +1,6 @@
+-- Problem: Show first name, last name, and the full province name of each patient.
+Example: 'Ontario' instead of 'ON'
+-- Difficulty: Easy
+
+Select first_name, last_name, province_name from patients 
+Join province_names on patients.province_id = province_names.province_id;
