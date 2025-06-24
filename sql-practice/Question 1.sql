1bb38e715779e56ccc990685453f5f923b9cf4d2
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-06-24 16:49:56 +0530

    Update Question 1.sql

--- sql-practice/Question 1.sql
+++ sql-practice/Question 1.sql
@@ -1,5 +1,4 @@
--- Problem: 1.
--- Show first name, last name, and gender of patients whose gender is 'M'
+-- Problem: Show first name, last name, and gender of patients whose gender is 'M'
 -- Difficulty: Easy
 
 SELECT first_name, last_name, gender FROM patients where gender = "M";
