3baf7dad84ab12eee88d46c0fa5af917dc3b5172
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-06-24 16:49:25 +0530

    Create Question 1.sql

---
+++ sql-practice/Question 1.sql
@@ -0,0 +1,5 @@
+-- Problem: 1.
+-- Show first name, last name, and gender of patients whose gender is 'M'
+-- Difficulty: Easy
+
+SELECT first_name, last_name, gender FROM patients where gender = "M";
