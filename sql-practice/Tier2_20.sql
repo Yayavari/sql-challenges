5853d9a11172b25ab96d5b8108c6f207c72aba5e
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 22:09:09 +0530

    Update Tier2_20.sql

--- sql-practice/Tier2_20.sql
+++ sql-practice/Tier2_20.sql
@@ -1,5 +1,5 @@
 -- Problem: For each doctor, display their id, full name, and the first and last admission date they attended.
 -- Difficulty: Medium
 
-SELECT doctor_id, concat(first_name,' ',last_name) AS full_name, min(admission_date) AS first_admission_date, max(admission_date) AS last_admission_date FROM admissions 
+SELECT doctor_id, CONCAT(first_name,' ',last_name) AS full_name, MIN(admission_date) AS first_admission_date, MAX(admission_date) AS last_admission_date FROM admissions 
 JOIN doctors ON admissions.attending_doctor_id=doctors.doctor_id GROUP BY doctor_id;
