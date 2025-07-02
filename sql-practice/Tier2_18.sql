d911af553709e6cd0faff0340acf1af8b170e43d
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 22:09:39 +0530

    Update Tier2_18.sql

--- sql-practice/Tier2_18.sql
+++ sql-practice/Tier2_18.sql
@@ -4,4 +4,4 @@
 -- Difficulty: Medium
 
 SELECT patient_id,attending_doctor_id,diagnosis FROM admissions 
-WHERE (patient_id%2=1 AND attending_doctor_id IN (1,5,19)) OR (attending_doctor_id LIKE '%2%' AND len(patient_id)=3);
+WHERE (patient_id%2=1 AND attending_doctor_id IN (1,5,19)) OR (attending_doctor_id LIKE '%2%' AND LEN(patient_id)=3);
