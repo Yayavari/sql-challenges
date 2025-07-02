221bd9d06e43a5d23820a8750a4d4088c3904f9d
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:02:09 +0530

    Update Tier1_13.sql

--- sql-practice/Tier1_13.sql
+++ sql-practice/Tier1_13.sql
@@ -1,4 +1,4 @@
 -- Problem: Show the patient id and the total number of admissions for patient_id 579.
 -- Difficulty: Easy
 
-SELECT patient_id, count(patient_id) from admissions where patient_id=579;
+SELECT patient_id, COUNT(patient_id) FROM admissions WHERE patient_id=579;
