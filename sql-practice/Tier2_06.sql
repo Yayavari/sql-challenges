b111981e1b29e2b0bac9d0a3f3d585eec4b849d8
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:10:06 +0530

    Create Tier2_06.sql

---
+++ sql-practice/Tier2_06.sql
@@ -0,0 +1,7 @@
+-- Problem: Show the total amount of male patients and the total amount of female patients in the patients table.
+-- Display the two results in the same row.
+-- Difficulty: Medium
+
+SELECT
+  (SELECT COUNT(*) FROM patients WHERE gender = 'M') AS male_count,
+  (SELECT COUNT(*) FROM patients WHERE gender = 'F') AS female_count;
