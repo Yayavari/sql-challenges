5f727d36bd1a512df1e8958823e38990a4c6fbc4
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:19:18 +0530

    Create Tier2_12.sql

---
+++ sql-practice/Tier2_12.sql
@@ -0,0 +1,4 @@
+-- Problem: Show all patient's first_name, last_name, and birth_date who were born in the 1970s decade. Sort the list starting from the earliest birth_date.
+-- Difficulty: Medium
+
+SELECT first_name,last_name, birth_date FROM patients WHERE YEAR(birth_date) BETWEEN 1970 AND 1979 ORDER BY birth_date ASC;
