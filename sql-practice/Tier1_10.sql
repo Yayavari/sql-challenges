ca906842f27793bceb1de1a151a9cdb644371a4e
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:01:14 +0530

    Update Tier1_10.sql

--- sql-practice/Tier1_10.sql
+++ sql-practice/Tier1_10.sql
@@ -1,4 +1,4 @@
 -- Problem: Show all columns for patients who have one of the following patient_ids: 1,45,534,879,1000
 -- Difficulty: Easy
 
-Select * from patients where patient_id in (1,45,534,879,1000);
+SELECT * FROM patients WHERE patient_id IN (1,45,534,879,1000);
