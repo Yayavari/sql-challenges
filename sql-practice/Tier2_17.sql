5ebbb71babd241635bd2b781a7d93e968d55f01f
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 22:09:56 +0530

    Update Tier2_17.sql

--- sql-practice/Tier2_17.sql
+++ sql-practice/Tier2_17.sql
@@ -1,4 +1,4 @@
 -- Problem: Show all columns for patient_id 542's most recent admission_date.
 -- Difficulty: Medium
 
-SELECT * FROM admissions WHERE patient_id=542 GROUP BY patient_id HAVING Max(admission_date);
+SELECT * FROM admissions WHERE patient_id=542 GROUP BY patient_id HAVING MAX(admission_date);
