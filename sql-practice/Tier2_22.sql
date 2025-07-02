243ca84a162419c8852aca3c24dcc90b297c099f
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:35:07 +0530

    Create Tier2_22.sql

---
+++ sql-practice/Tier2_22.sql
@@ -0,0 +1,6 @@
+-- Problem: For every admission, display the patient's full name, their admission diagnosis, and their doctor's full name who diagnosed their problem.
+-- Difficulty: Medium
+
+SELECT CONCAT(patients.first_name,' ',patients.last_name) AS patient_name, diagnosis, concat(doctors.first_name,' ',doctors.last_name) AS doctor_name FROM admissions
+JOIN patients ON admissions.patient_id=patients.patient_id
+JOIN doctors ON admissions.attending_doctor_id=doctors.doctor_id;
