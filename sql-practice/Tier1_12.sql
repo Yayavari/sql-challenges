b563760e1808f24fed32b9bbc4c321dcfae1d255
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:01:48 +0530

    Update Tier1_12.sql

--- sql-practice/Tier1_12.sql
+++ sql-practice/Tier1_12.sql
@@ -1,4 +1,4 @@
 -- Problem: Show all the columns from admissions where the patient was admitted and discharged on the same day.
 -- Difficulty: Easy
 
-SELECT * FROM admissions where admission_date=discharge_date;
+SELECT * FROM admissions WHERE admission_date=discharge_date;
