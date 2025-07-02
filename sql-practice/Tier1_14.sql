be05944365df740acf688a206a84bbda4bf94ba9
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:02:33 +0530

    Update Tier1_14.sql

--- sql-practice/Tier1_14.sql
+++ sql-practice/Tier1_14.sql
@@ -1,4 +1,4 @@
 -- Problem: Based on the cities that our patients live in, show unique cities that are in province_id 'NS'.
 -- Difficulty: Easy
 
-SELECT distinct city from patients where province_id='NS';
+SELECT DISTINCT city FROM patients WHERE province_id='NS';
