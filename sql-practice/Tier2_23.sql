455b570d9361060a29df3ddbdc174e9bf8b78aa5
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:37:51 +0530

    Create Tier2_23.sql

---
+++ sql-practice/Tier2_23.sql
@@ -0,0 +1,5 @@
+-- Problem: display the first name, last name and number of duplicate patients based on their first name and last name.
+-- Ex: A patient with an identical name can be considered a duplicate.
+-- Difficulty: Medium
+
+SELECT first_name,last_name, COUNT(*) AS num_of_duplicates FROM patients GROUP BY first_name,last_name HAVING COUNT(*)>1;
