f16e874c73060464333b7cf37d5d0ade830a5bcc
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 22:12:37 +0530

    Update Tier1_06.sql

--- sql-practice/Tier1_06.sql
+++ sql-practice/Tier1_06.sql
@@ -1,4 +1,4 @@
 -- Problem: Show first name and last name concatinated into one column to show their full name.
 -- Difficulty: Easy
 
-SELECT concat(first_name, ' ', last_name) AS full_name FROM patients;
+SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM patients;
