57a18464698f5b0577aee7a611d51d298f0bf1a0
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:00:46 +0530

    Update Tier1_09.sql

--- sql-practice/Tier1_09.sql
+++ sql-practice/Tier1_09.sql
@@ -1,4 +1,4 @@
 -- Problem: Show the first_name, last_name, and height of the patient with the greatest height.
 -- Difficulty: Easy
 
-Select first_name, last_name, max(height) from patients;
+SELECT first_name, last_name, max(height) FROM patients;
