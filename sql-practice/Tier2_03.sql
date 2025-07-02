66830aa7d52095d3be0aa4bd6e29242a3a8942b0
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:04:50 +0530

    Create Tier2_03.sql

---
+++ sql-practice/Tier2_03.sql
@@ -0,0 +1,4 @@
+-- Problem: Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+-- Difficulty: Medium
+
+SELECT patient_id, first_name FROM patients WHERE first_name LIKE 's%' AND first_name LIKE '%s' AND len(first_name)>=6;
