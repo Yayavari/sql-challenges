980da137491aae0fc12c636c91fd8d63d15b1ede
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 21:00:26 +0530

    Update Tier1_08.sql

--- sql-practice/Tier1_08.sql
+++ sql-practice/Tier1_08.sql
@@ -1,4 +1,4 @@
 -- Problem: Show how many patients have a birth_date with 2010 as the birth year.
 -- Difficulty: Easy
 
-Select count(birth_date) from patients where Year(birth_date)=2010;
+SELECT COUNT(birth_date) FROM patients WHERE YEAR(birth_date)=2010;
