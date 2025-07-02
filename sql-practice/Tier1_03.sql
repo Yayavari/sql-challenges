f5c57315e03928c73f0e951b5bb554a62fa3e1e3
Yayavari <159180688+Yayavari@users.noreply.github.com>
2025-07-02 20:57:55 +0530

    Update Tier1_03.sql

--- sql-practice/Tier1_03.sql
+++ sql-practice/Tier1_03.sql
@@ -1,4 +1,4 @@
 -- Problem: Show first name of patients that start with the letter 'C'
 -- Difficulty: Easy
 
-SELECT first_name FROM patients where first_name like 'C%';
+SELECT first_name FROM patients WHERE first_name LIKE 'C%';
